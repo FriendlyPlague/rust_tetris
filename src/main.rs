7ed9fb7210b89f67a0f63c887a8447df7e6ceda4
Zachary Gillmore <zachgillmore1@gmail.com>
2023-08-17 09:28:28 -0700

    created outline

--- src/main.rs
+++ src/main.rs
@@ -5,37 +5,98 @@ const HEIGHT: usize = 20;
 struct Color(u8, u8, u8);
 
 fn main() {
-    let mut grid = tetris_grid {grid: [None;200]};
-    grid.draw_blocks();
+    let the_grid = TetrisGrid { grid: [None; 200] };
+    draw_blocks(&the_grid.grid);
 }
 
-struct tetris_grid {
+enum Piece {
+    O,
+    I,
+    S,
+    Z,
+    L,
+    J,
+    T,
+}
+
+impl Piece {
+    pub fn new (&self) -> (Color, [Option<Color>; 9]) {
+        (self.color(), self.generate_piece())
+    }
+
+    fn color(&self) -> Color {
+        match *self {
+            Piece::O => Color(208, 245, 22),
+            Piece::I => Color(9, 180, 214),
+            Piece::S => Color(232, 12, 15),
+            Piece::Z => Color(5, 153, 24),
+            Piece::L => Color(245, 178, 22),
+            Piece::J => Color(240, 31, 205),
+            Piece::T => Color(113, 6, 158),
+        }
+    }
+
+    #[rustfmt::skip]
+    fn generate_piece(&self) -> [Option<Color>; 9] {
+        let c = self.color();
+        match self {
+            Piece::I => [None, Some(c), None,
+                        None, Some(c), None,
+                        None, Some(c), None],
+            Piece::O => [Some(c),Some(c),None,
+                        Some(c),Some(c),None,
+                        None, None, None],
+            Piece::S => [None,  Some(c),Some(c),
+                        Some(c), Some(c), None,
+                        None,None,None],
+            Piece::Z =>[Some(c),Some(c), None,
+                        None, Some(c), Some(c),
+                        None, None, None],
+            Piece::L => [Some(c),None,None,
+                        Some(c), None, None,
+                        Some(c),Some(c),None],
+            Piece::J => [None, None,Some(c),
+                        None, None, Some(c),
+                        None,Some(c),Some(c)],
+            Piece::T => [Some(c),Some(c),Some(c),
+                        None, Some(c), None,
+                        None, None, None],
+        }
+    }
+}
+
+fn rotate_right(p: Piece, area: &mut [Option<Color>; 9]) {
+    if !matches!(Piece::O, p) {
+        let mut new_area: [Option<Color>; 9] = [None; 9]; //think array of 3 by 3
+        for x in 0..3 {
+            new_area[x * 3 + 2] = area[x]; // rotates top 3 to right 3
+        }
+        for y in 0..3 {
+            new_area[8 - y] = area[y * 3 + 2]; //rotates right 3 to bottom 3
+        }
+        for x in 0..3 {
+            new_area[x * 3] = area[6 + x]; //rotates bottom 3 to left 3
+        }
+        for y in 0..3 {
+            new_area[2 - y] = area[y * 3]; //rotates left 3 to top 3
+        }
+        *area = new_area;
+    }
+}
+
+
+
+struct TetrisGrid {
     grid: [Option<Color>; 200],
 }
 
-impl tetris_grid {
+impl TetrisGrid {
     fn delete_rows(&mut self, start_r: usize, end_r: usize) {
         /* Clears rows then shifts everything above cleared rows down*/
         for i in WIDTH * start_r..WIDTH * end_r {
             self.grid[i] = None;
         }
         self.grid[0..WIDTH * end_r].rotate_right(WIDTH * (end_r - start_r));
-    }
-
-    fn draw_blocks(&self) {
-        for y in 0..HEIGHT {
-            for x in 0..WIDTH {
-                tetris_grid::draw_block(self.grid[x * y], x, y)
-            }
-            println!();
-        }
-    }
-
-    fn draw_block(color: Option<Color>, x: usize, y: usize) {
-        match color {
-            Some(_) => print!("X"),
-            None => print!("O"),
-        }
     }
 
     fn check_lines(&self) -> Option<(usize, usize)> {
@@ -63,4 +124,18 @@ impl tetris_grid {
         }
     }
 }
-    
+fn draw_blocks(grid: &[Option<Color>; WIDTH * HEIGHT]) {
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            draw_block(grid[x * y], x, y)
+        }
+        println!();
+    }
+}
+
+fn draw_block(color: Option<Color>, x: usize, y: usize) {
+    match color {
+        Some(_) => print!("X"),
+        None => print!("O"),
+    }
+}
